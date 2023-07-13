9c31ee7f7a79cf69c52505d7335cd643c796b97e
Berrypie2706 <aniket.chouksey444@gmail.com>
2023-07-13 15:40:44 +0530

    Second commit

---
+++ Assignment_4/Assignment_4/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //            //Write a C# program that prompts the user to enter a number.
+            //            Implement decision-making constructs to perform the following tasks:
+            //a.If the number is positive, display "The number is positive."
+            //b.If the number is negative, display "The number is negative."
+            //c.If the number is zero, display "The number is zero."
+            //Utilize the conditional operator to determine whether the number is even or odd.
+            //a.If the number is even, display "The number is even."
+            //b.If the number is odd, display "The number is odd."
+
+
+            int num;
+            Console.WriteLine("Enter number");
+            num = int.Parse(Console.ReadLine());
+            if(num> 0)
+            {
+                Console.WriteLine("Number is positive");
+            }
+           else if(num<0) {
+                Console.WriteLine("Number is negative");
+            }
+            else
+            {
+                Console.WriteLine("Number is zero");
+            }
+
+
+            //Utilize the conditional operator to determine whether the number is even or odd.
+            //(Condition) ? true case : false case
+            var res = (num % 2 == 0) ? "Number is even" : "Number is odd";
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Assignment_5/Assignment_5/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //            Write a C# program to create a simple login system.
+            //Implement decision - making constructs to perform the following tasks:
+            //a.Prompt the user to enter a username and password.
+            //b.If both the username and password are correct, display "Login successful."
+            //c.If the username is correct but the password is incorrect, display "Invalid password."
+            //d.If the username is incorrect, display "Invalid username."
+            //If the user fails to provide the correct login credentials within
+            //three attempts, terminate the program and display "Login failed."
+
+
+            string validUserName = "admin1307";
+            string validPassword = "admin@1307";
+            int remainingAttempts = 3;
+            
+            while (remainingAttempts > 0)
+            {
+                string userName, password;
+                Console.Write("Enter UserName");
+                userName = Console.ReadLine();
+                Console.Write("Enter Password");
+                password = Console.ReadLine();
+
+                if (userName == validUserName && password == validPassword)
+                {
+                    Console.WriteLine("Login successfull");
+                    break;
+
+
+                }
+                else if (userName == validUserName)
+                {
+                    Console.WriteLine("Invalid Password");
+                }
+                else
+                {
+                    Console.WriteLine("Invlaid Username");
+                }
+                remainingAttempts--;
+                Console.WriteLine();
+            }
+            Console.WriteLine("Login failed");
+           
+            Console.ReadKey();
+            }
+
+        }
+   }
+
