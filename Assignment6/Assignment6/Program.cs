3ce7cd998e995a88e972e68ee00c3a198051b8a9
Berrypie2706 <aniket.chouksey444@gmail.com>
2023-07-14 18:36:58 +0530

    Third commit

---
+++ Assignment6/Assignment6/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //In this assignment, you will work with jagged arrays in C# to manipulate data.
+            //You will perform various operations such as creating jagged arrays, accessing elements, and calculating averages.
+            //Create a jagged array called studentScores to store the scores of three students.Each student can have a different number of scores.
+            //            Initialize the studentScores array with the following scores:
+
+            //Student 1: 85, 92, 78
+            //Student 2: 90, 87, 93, 89
+            //Student 3: 76, 88
+            //Print the scores of each student using nested loops.
+
+            //Calculate the average score for each student and print the results.
+
+            //Calculate the average score for all the students combined and print the result.
+
+            int[][] studentScores = new int[3][];
+            studentScores[0] = new int[3] { 85, 92, 78 };
+            studentScores[1] = new int[4] { 90, 87, 93, 89 };
+            studentScores[2] = new int[2] { 76, 88 };
+            for (int i = 0; i < studentScores.Length; i++)
+            {
+
+                Console.Write($"Student{i + 1}\'s score : ");
+
+
+                for (int j = 0; j < studentScores[i].Length; j++)
+                {
+
+
+                    Console.Write(studentScores[i][j] + ",");
+
+
+                }
+                Console.WriteLine("\n");
+            }
+                //average score for each student and print results
+                    for (int i = 0; i < studentScores.Length; i++)
+                    {
+                        double sum = 0;
+
+                        for (int j = 0; j < studentScores[i].Length; j++)
+                        {
+                            sum += studentScores[i][j];
+                        }
+
+                        double average = sum / studentScores[i].Length;
+                        Console.WriteLine($"Average score for Student {i + 1}: {average}");
+                    }
+
+                //average score if all students and print result
+                double totalSum = 0;
+                int totalScores = 0;
+
+                for (int i = 0; i < studentScores.Length; i++)
+                {
+                    for (int j = 0; j < studentScores[i].Length; j++)
+                    {
+                        totalSum += studentScores[i][j];
+                        totalScores++;
+                    }
+                }
+
+                double totalAverage = totalSum / totalScores;
+                Console.WriteLine($"Average score for all students combined: {totalAverage}");
+
+
+
+            Console.ReadKey();
+        }
+
+            
+        }
+
+    }
+
---
+++ Assignment7/Assignment7/Program.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Assignment7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //        Task:
+            //            Create an ArrayList and add the following elements to it: "apple," "banana," "cherry," "date," "elderberry."
+
+            //Print the total number of elements in the ArrayList. Write the code to insert,remove and search items from the list.
+            //Sample Task to check Input Output: 
+            //Check if the ArrayList contains the element "date" and print the result.
+            //Insert the element "fig" at the second position in the ArrayList.
+            //Remove the element "banana" from the ArrayList.
+            //Print all the elements in the ArrayList using a loop.
+
+            //ArrayList arrayList = new ArrayList();
+            //Console.WriteLine("How many items you want to add?");
+            //int noOfItems = int.Parse(Console.ReadLine());
+            //string item;
+
+            //Console.WriteLine("Enter item you want to add");
+            //for (int i = 0; i < noOfItems; i++)
+            //{
+            //    item = Console.ReadLine();
+            //    arrayList.Add(item.ToLower());
+
+            //}
+            //Console.WriteLine("List of items: \n");
+            //foreach (var i in arrayList)
+            //{
+            //    Console.WriteLine(i);
+
+            //}
+
+            ////Check if the ArrayList contains the element "date" and print the result.
+            //Console.WriteLine("Check if the ArrayList contains the element \"date\" and print the result.\n");
+            //Console.WriteLine("Enter the name of the item you want to search:\n");
+
+
+            //for (int i = 0; i < noOfItems; i++)
+            //{
+
+            //    string search = Console.ReadLine();
+            //    bool itemExists = arrayList.Contains(search.ToLower());
+            //    if (itemExists)
+            //    {
+            //        Console.WriteLine($"The Item '{search}' is at position {i+1} in the list.");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Not found");
+            //    }
+
+
+            //    // Insert the element "fig" at the second position in the ArrayList.
+            //    Console.WriteLine("Insert the element \"fig\" at the second position in the ArrayList.");
+            //    Console.WriteLine("Enter the item you want to insert");
+
+            //    string addItem = Console.ReadLine();
+            //    Console.WriteLine("In which position you want to insert");
+            //    int pos = int.Parse(Console.ReadLine());
+            //    arrayList.Insert(pos - 1, addItem.ToLower());
+            //    Console.WriteLine("Updated lists of fruits");
+            //    foreach (string a in arrayList)
+            //    {
+            //        Console.WriteLine(a);
+            //    }
+
+            //    //Remove the element "banana" from the ArrayList
+            //    Console.WriteLine("Remove the element \"banana\" from the ArrayList\n");
+            //    Console.WriteLine("Enter the item you want to remove:\n");
+            //    string removeItem = Console.ReadLine();
+
+            //    arrayList.Remove(removeItem.ToLower());
+
+            //    Console.WriteLine("Item is deleted");
+            //    Console.WriteLine("New list of items \n");
+            //    foreach (string a in arrayList)
+            //    {
+
+            //        Console.WriteLine(a);
+            //    }
+
+               
+                //array list add elements
+                ArrayList fruits = new ArrayList();
+                fruits.Add("apple");
+                fruits.Add("banana");
+                fruits.Add("cherry");
+                fruits.Add("date");
+                fruits.Add("elderberry");
+
+            Console.WriteLine("Number of elements in the list:" + fruits.Count);
+
+            Console.WriteLine("Fruits in the list");
+            foreach (var fruit in fruits)
+            {
+                Console.WriteLine(fruit);
+            }
+
+            Console.WriteLine("Enter item you want to search");
+            string searchFruit = Console.ReadLine();
+
+
+            int index = fruits.IndexOf(searchFruit);
+            if (index == -1)
+            {
+                Console.WriteLine("Not found!!!!");
+
+
+            }
+            else
+            {
+                Console.WriteLine("Item is exist in the list");
+
+            }
+
+
+            Console.WriteLine("Enter fruit you want to add in the list");
+            string addFruit = Console.ReadLine();
+            fruits.Insert(1, addFruit);
+            Console.WriteLine("List after adding fruit:");
+            foreach (var fruit in fruits) { Console.WriteLine(fruit); }
+
+
+            Console.WriteLine("Enter item you want to delete");
+            string removeItem = Console.ReadLine();
+            int index1 = fruits.IndexOf(removeItem);
+            
+            if(index1 == -1)
+            {
+                Console.WriteLine("No such item is in the list");
+            }
+            else
+            {
+                fruits.Remove(removeItem);
+                Console.WriteLine("List of fruits after removing:");
+                foreach (var a in fruits) { Console.WriteLine(a); }
+            }
+            
+
+
+            Console.ReadKey();
+            }
+
+
+        }
+    }
+
+
