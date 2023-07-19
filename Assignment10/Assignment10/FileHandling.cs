4fc97ae132d9c150e9572e2b7334f556d94f902b
Berrypie2706 <aniket.chouksey444@gmail.com>
2023-07-19 15:58:05 +0530

    Sixth commit

---
+++ Assignment10/Assignment10/FileHandling.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Assignment10
+{
+   public class FileHandling
+    {
+        public void Create(string fileName, string content)
+        {
+
+            try
+            {
+                //Console.WriteLine("Enter file path");
+                //string Path = Console.ReadLine();
+                //string fPath = Path + fileName;
+
+                if (File.Exists(fileName))
+                {
+                    Console.WriteLine("File Already there");
+                }
+
+                else
+                {
+                    StreamWriter streamWriter = File.AppendText(fileName);
+                    streamWriter.WriteLine(content);
+                    streamWriter.Dispose();
+                    streamWriter.Close();
+                    Console.WriteLine("File is Created and Content is added");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error While Creating the file: {ex.Message}");
+            }
+
+        }
+        public void Read(string fileName) 
+        {
+            try
+            {
+                
+                if(File.Exists(fileName))
+                {
+                    string[] lines = File.ReadAllLines(fileName);
+                    foreach(string line in lines)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("File not exists");
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Error while reading the file: {ex.Message}");
+            }
+        }
+        public void AppendToFile(string fileName,string content)
+        {
+            try
+            {
+
+                StreamReader sr = new StreamReader(content);
+                string text = "";
+                while((text = sr.ReadLine())!=null)
+                {
+                    Console.WriteLine(text);
+                }               
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error while Appending the file: {ex.Message}");
+
+            }
+        }
+        public void DeleteFile(string fileName)
+        {
+            try
+            {
+                if (File.Exists(fileName))
+                {
+                    File.Delete(fileName); Console.WriteLine("File is deleted successfully");
+                }
+                else
+                {
+                    Console.WriteLine($"File '{fileName}' does not exist.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error while Appending the file: {ex.Message}");
+
+            }
+        }
+    }
+}
---
+++ Assignment10/Assignment10/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FileHandling file = new FileHandling();
+            Console.WriteLine("Choose operation:");
+            Console.WriteLine("1. Create");
+            Console.WriteLine("2. Read");
+            Console.WriteLine("3. Append");
+            Console.WriteLine("4. Delete");
+
+            int operations = int.Parse(Console.ReadLine()); 
+            
+            switch(operations)
+            {
+                case 1:
+                    Console.WriteLine("Enter File name");
+                    string filename = Console.ReadLine();
+                    Console.WriteLine("Enter the content");
+                    string content = Console.ReadLine();
+                    file.Create(filename, content);
+                    break;
+
+                    case 2:
+                    Console.WriteLine("Enter The Name Of the File you Want To Read");
+                    string readFilename = Console.ReadLine();
+                    file.Read(readFilename);
+                    break;
+
+                    case 3:
+                    Console.WriteLine("Enter the File Name to Append");
+                    string appendFile = Console.ReadLine();
+                    Console.WriteLine("Enter The Text You Want To Append");
+                    string appendedText = Console.ReadLine();   
+                    file.AppendToFile(appendFile,appendedText);
+                    break;
+
+                    case 4:
+                    Console.WriteLine("Enter the File Name You Want To Delete");
+                    string Delete = Console.ReadLine();
+                    file.DeleteFile(Delete); 
+                    break;
+
+
+            }
+        }
+    }
+    
+}
+
