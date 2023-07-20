2b30eff5648ecb5b40271c0817410c0204ce6cf5
Berrypie2706 <aniket.chouksey444@gmail.com>
2023-07-20 15:59:27 +0530

    Seventh commit

---
+++ Assignment11/Assignment11/LargeDataCollection.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment11
+{
+    public class LargeDataCollection <L> : IDisposable
+    {
+        private List<L> Data;
+        public LargeDataCollection(List<L> initialData)
+        {
+            Data = new List<L>(initialData);
+        }
+        public void Add(L item)
+        {
+            Data.Add(item);
+        }
+        public void Remove(L item)
+        {
+            Data.Remove(item);
+        }
+        public L GetItem(int index)
+        {
+            if (index >= 0 && index < Data.Count)
+            {
+                return Data[index];
+            }
+            throw new IndexOutOfRangeException("Index is out of range.");
+        }
+                
+        public void Dispose()
+        {
+            Data = null;
+            Console.WriteLine("The resources has been released from the collection");
+        }
+        public void showData()
+        {
+            Console.WriteLine("Total elements are :"+Data.Count);
+            for(int i=0; i<Data.Count; i++)
+            {
+                Console.WriteLine($"Index {i} : {Data[i]}");
+            }
+        }
+    }
+}
---
+++ Assignment11/Assignment11/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<int> initialData = new List<int> { 1, 2, 3, 4, 5, 6, 7, 60, 70, 80, 90, 100, 110, 120 };
+                LargeDataCollection<int> largeDataCollection = new LargeDataCollection<int>(initialData);
+                largeDataCollection.Add(130);
+                largeDataCollection.Add(140);
+                largeDataCollection.Add(150);
+                Console.WriteLine("The list after Adding The Elements");
+                largeDataCollection.showData();
+                Console.WriteLine("\n");
+
+                //removing elements
+
+                largeDataCollection.Remove(1);
+                largeDataCollection.Remove(150);
+                largeDataCollection.Remove(90);
+
+                //Accessing List After Removing elements
+                Console.WriteLine("The Updated list after Removing The Elements");
+                largeDataCollection.showData();
+                Console.WriteLine("\n");
+
+                //calling the `Dispose` method explicitly
+
+                Console.WriteLine("*****Calling the Dipose method to Release the resources of the collection.******* \n");
+              
+                largeDataCollection.Dispose();
+                Console.WriteLine("\n");
+                largeDataCollection.showData();
+               
+            } catch (NullReferenceException e) 
+            {
+                Console.WriteLine("------------------------------------------------------------------------------------------------------");
+                Console.WriteLine("---->>>\tNull Reference exception: " + e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error!!!" + e.Message);
+            }
+
+          
+            Console.ReadKey();
+
+        }
+    }
+}
