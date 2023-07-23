bd3ec03b34ad87863f52ed1eecbcdf0db8f31d31
Berrypie2706 <aniket.chouksey444@gmail.com>
2023-07-23 21:29:03 +0530

    Eight commit

---
+++ Assignment12/Assignment12/InputValidation.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Assignment12
+{
+    public class InputValidation
+    {
+        public int CountWords(string inputText)
+        {
+            string[] words = inputText.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            return words.Length;
+        }
+        public string[] EmailValidation(string inputText)
+        {
+
+            string pattern = @"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b";
+
+            // Create a Regex object with the pattern
+            Regex regex = new Regex(pattern);
+
+            // Use the Regex.Matches method to find all matches in the input text
+            MatchCollection matches = regex.Matches(inputText);
+
+            // Convert the matches to an array of strings
+            string[] emailAddresses = new string[matches.Count];
+            for (int i = 0; i < matches.Count; i++)
+            {
+                emailAddresses[i] = matches[i].Value;
+            }
+            return emailAddresses;
+        }
+
+        public string[] IsMobileValid(String inputText)
+        {
+            string pattern = @"\b\d{10}\b";
+            Regex regex = new Regex(pattern);
+            MatchCollection matches = regex.Matches(inputText);
+            string[] mobileAddresses = new string[matches.Count];
+            for (int i = 0; i < matches.Count; i++)
+            {
+                mobileAddresses[i] = matches[i].Value;
+            }
+            return mobileAddresses;
+        }
+        public string[]  PerformRegexSearch(string inputText, string customRegexPattern)
+        {
+            Regex regex = new Regex(customRegexPattern);
+
+            // Use the Regex.Matches method to find all matches in the input text
+            MatchCollection matches = regex.Matches(inputText);
+
+            // Convert the matches to an array of strings
+            string[] customRegexMatches = new string[matches.Count];
+            for (int i = 0; i < matches.Count; i++)
+            {
+                customRegexMatches[i] = matches[i].Value;
+            }
+
+            return customRegexMatches;
+
+
+
+        }
+    }
+}
---
+++ Assignment12/Assignment12/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Assignment12
+{
+          internal class Program
+    {
+        static void Main(string[] args)
+        {
+            InputValidation inputValidation = new InputValidation();
+            Console.WriteLine("Enter Text:");
+            string inputText = Console.ReadLine();
+            int count = inputValidation.CountWords(inputText);
+            Console.WriteLine("Word Counts:"+count);
+
+            var emailAddresses = inputValidation.EmailValidation(inputText);
+            Console.WriteLine($"Email address found: {emailAddresses}");
+            foreach( var emailAddress in emailAddresses )
+            {
+                Console.WriteLine(emailAddress);
+            }
+
+            var mobileNumbers = inputValidation.IsMobileValid(inputText);
+            Console.WriteLine($"Mobile numbers found: {mobileNumbers}");
+            foreach( var mobileNumber in mobileNumbers)
+            {
+                Console.WriteLine(mobileNumber);
+            }
+
+            Console.WriteLine("\nEnter a custom regular expression:");
+            string customRegexPattern = Console.ReadLine();
+
+            var customRegexMatches = inputValidation.PerformRegexSearch(inputText, customRegexPattern);
+            Console.WriteLine("\nCustom regex matches found:");
+            foreach (var match in customRegexMatches)
+            {
+                Console.WriteLine(match);
+            }
+
+
+            Console.ReadKey();  
+
+        }
+
+
+        }
+    }
+    
+
+
