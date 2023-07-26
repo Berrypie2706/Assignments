7e44fa0132e4e66ff6b87fa0643eb8f4a9db4a5f
Berrypie2706 <aniket.chouksey444@gmail.com>
2023-07-26 18:26:33 +0530

    Twelth commit

---
+++ Assignment16/Assignment16/BankAccount.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment16
+{
+    public class BankAccount
+    {
+        private static int nextAccountNumber =100;
+       
+
+
+        public int AccountNumber { get; }
+        public string AccountHolderName { get; }
+        private double Balance { get; set; }
+
+        public BankAccount(String accountHolderName)
+        {
+            AccountNumber= nextAccountNumber++;
+            AccountHolderName = accountHolderName;
+
+            Balance = 0;
+
+        }
+
+        public void Deposit(double depositAmount)
+        {
+           
+            if (depositAmount <= 0)
+            {
+                throw new ArgumentException("Deposit must be greater than zero");
+            }
+            else
+            {
+                Balance += depositAmount;
+            }
+           
+            
+        }
+
+        public void Withdraw( double amount)
+        {
+            if(amount>Balance )
+            {
+                Console.WriteLine("Insufficient Balance");
+            }
+            if(amount<=0)
+            {
+                Console.WriteLine("Withdrwal must be greater than zero");
+            }
+            Balance -=amount;
+            Console.WriteLine("Withdrawl successfully!!!");
+        }
+        public void PrintAccountDetails()
+        {
+            Console.WriteLine($"Account Number: {AccountNumber}");
+            Console.WriteLine($"Account Holder Name: {AccountHolderName}");
+            Console.WriteLine($"Balance: {Balance}");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Assignment16/Assignment16/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Console.WriteLine("Enter Account holder name");
+                string accountHolderName = Console.ReadLine();
+                BankAccount account = new BankAccount(accountHolderName);
+                Console.WriteLine("Enter the amount you want to deposit");
+                double depAmount = Convert.ToDouble(Console.ReadLine());
+                account.Deposit(depAmount);
+                account.PrintAccountDetails();
+
+                Console.WriteLine("Enter the amount you want to withdraw");
+                double withdAmount = Convert.ToDouble(Console.ReadLine());  
+                account.Withdraw(withdAmount); 
+                account.PrintAccountDetails();
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine("Format Exception: " + e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unknown Error!!!" + e.Message);
+                Console.ReadKey();
+            }
+
+
+
+        }
+    }
+}
