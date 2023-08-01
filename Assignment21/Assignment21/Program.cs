8171c7c9669f677ce02f069e0a391ea156864726
Berrypie2706 <aniket.chouksey444@gmail.com>
2023-08-01 15:58:50 +0530

    First commit

---
+++ Assignment21/Assignment21/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookLibrary;
+
+namespace Assignment21
+{
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            string choice;
+            do
+            {
+                Console.WriteLine("Choose from below:");
+                Console.WriteLine("1. View Book Library \n 2. Add Book \n " +
+                    "3. Search Book By Title \n 4. Search Book By ID");
+
+                int a = int.Parse(Console.ReadLine());
+                BookLib bookLib = new BookLib();
+
+                switch (a)
+                {
+                    case 1:
+                        {
+                           
+                                bookLib.Print();                            
+                                break;
+                        }
+                    case 2:
+                        {
+                            try
+                            {
+                                Book book = new Book();
+                                Console.WriteLine("Enter Book Id, Book Title, Book Author, Book Genre:");
+                                book.BookId = int.Parse(Console.ReadLine());
+                                book.Title = Console.ReadLine();
+                                book.Author = Console.ReadLine();
+                                book.Genre = Console.ReadLine();
+                                bookLib.AddBook(book);
+                            }
+                            catch(FormatException e)
+                            {
+                                Console.WriteLine("Format Exception:" + e.Message);
+                            }
+                            catch(Exception e)
+                            {
+                                Console.WriteLine("SomeThing Went Wrong:" +e.Message);
+                            }
+                          
+                            break;
+                        }
+                    case 3:
+                        {
+                            Console.WriteLine("Enter the Book Title You Want To Search");
+                            Book book = new Book();
+                            book.Title = Console.ReadLine();
+                            bookLib.searchBookByTitle(book.Title);
+                            break;
+                        }
+                    case 4:
+                        {
+                            Console.WriteLine("Enter the Book ID You Want To Search");
+                            Book book = new Book();
+                            book.BookId = int.Parse(Console.ReadLine());
+                            bookLib.searchBookById(book.BookId);
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Invalid Operation");
+                            break;
+                        }
+
+                }
+                Console.WriteLine("Press Y to Continue Otherwise Press Enter To Exit");
+                choice = Console.ReadLine();
+            }
+ 
+            while (choice == "Y" || choice == "y");
+            Console.ReadKey();
+            }
+        }
+    }
+
---
+++ Assignment21/BookLibrary/BookLibrary/BookLib.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Collections.Specialized.BitVector32;
+
+namespace BookLibrary
+{
+    public class BookLib
+    {
+        static List<Book> library;
+        public BookLib()
+        {
+            library = new List<Book>()
+            {
+                new Book() {BookId = 001,Title = "To Kill a MockingbirdAuthor", Author =  "Harper Lee", Genre = "Fiction, Classic, Coming-of-age" },
+                new Book() {BookId = 002,Title = "1984", Author =  " George Orwell", Genre = "Fiction, Dystopian, Political" },
+                new Book() {BookId = 003,Title = "The Great Gatsby", Author =  "F. Scott Fitzgerald", Genre = "Fiction, Classic, Romance" },
+                new Book() {BookId = 004,Title = "Pride and Prejudice", Author =  "Jane Austen", Genre = " Fiction, Classic, Romance" },
+                new Book() {BookId = 005,Title = " The Lord of the Rings", Author =  " J.R.R. Tolkien", Genre = "Fantasy, Adventure" },
+                new Book() {BookId = 006,Title = "Harry Potter and the Sorcerer's Stone", Author =  "J.K. Rowling", Genre = " Fantasy, Young Adult" },
+              
+
+            };
+        }
+
+        public void Print()
+        {
+            foreach (Book book in library)
+            {
+                Console.WriteLine("Book ID: \t" + book.BookId);
+                Console.WriteLine("Title: \t" + book.Title);
+                Console.WriteLine("Author: \t" + book.Author);
+                Console.WriteLine("Genre: \t" + book.Genre);
+
+            }
+        }
+
+        public void AddBook(Book book)
+        {
+            library.Add(book);
+            Console.WriteLine("Updated Library");
+            this.Print();
+        }
+
+        public void searchBookById(int bookid)
+        {
+            var book = library.Find(b => b.BookId == bookid);
+            if (book != null)
+            {
+                Console.WriteLine("Book is Available");
+                Console.WriteLine("------------------------------");
+                Console.WriteLine("Book ID: \t" + book.BookId);
+                Console.WriteLine("Title: \t" + book.Title);
+                Console.WriteLine("Author: \t" + book.Author);
+                Console.WriteLine("Genre: \t" + book.Genre);
+
+            }
+
+            else
+            {
+                Console.WriteLine("Not available");
+            }
+        }
+         public void searchBookByTitle(string title)
+          {
+                    
+            var book = library.Find(b => b.Title.Equals(title, StringComparison.OrdinalIgnoreCase));
+            if (book != null)
+            {
+
+                Console.WriteLine("Book is Available");
+                Console.WriteLine("------------------------------");
+                Console.WriteLine("Book ID: \t" + book.BookId);
+                Console.WriteLine("Title: \t" + book.Title);
+                Console.WriteLine("Author: \t" + book.Author);
+                Console.WriteLine("Genre: \t" + book.Genre);
+            }
+                
+                else
+                {
+                    Console.WriteLine("Not available");
+                }
+            }
+
+        }
+    }
+
