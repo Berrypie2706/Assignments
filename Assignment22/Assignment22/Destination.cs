a4d5477dd569567f1ba09b3d8e8e7dda9c397f07
Berrypie2706 <aniket.chouksey444@gmail.com>
2023-08-03 17:19:25 +0530

    First Commit mapping

---
+++ Assignment22/Assignment22/Destination.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment22
+{
+    public class Destination
+    {
+       public int ID { get; set; }
+        public string Name1 { get; set; }
+        public int Age { get; set; }
+        public double Salary { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ Assignment22/Assignment22/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment22
+{
+    public class Program
+    {
+        public static void MapProperties(object Source, object Destination)
+        {
+            Type type = Source.GetType();
+            Type type1 = Destination.GetType();
+
+            PropertyInfo[] sourceProperties = type.GetProperties();
+            PropertyInfo[] destProperties = type1.GetProperties();
+
+            foreach(PropertyInfo sourceProperty in sourceProperties)
+            {
+                PropertyInfo destProperty = Array.Find(destProperties,p => p.Name == sourceProperty.Name && p.PropertyType == sourceProperty.PropertyType);
+             if(destProperty != null && destProperty.CanWrite)
+                {
+                    destProperty.SetValue( Destination, sourceProperty.GetValue(Source));
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Source source = new Source()
+            {
+              //  ID = 1,
+                Name = "Test",
+                Age = 20
+            };
+            Destination source2 = new Destination();
+
+            MapProperties(source, source2);
+
+            Console.WriteLine("Mapping successful. Destination properties:");
+            Console.WriteLine($"Id: {source2.ID}");
+            Console.WriteLine($"Name: {source2.Name1}");
+            Console.WriteLine($"Age: {source2.Age}");
+            Console.WriteLine($"Salary: {source2.Salary}");
+            Console.WriteLine($"Address: {source2.Address}");
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
